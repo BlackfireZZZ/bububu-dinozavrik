24b19c2759d2868d630dcdf9d265e1086010dcc7
BlackfireZZZ <Stipundrik@gmail.com>
2024-07-21 21:57:56 +0300

    Init2

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+// src/App.js
+import React from 'react';
+import Slider from './components/Slider';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <Slider />
+        </div>
+    );
+}
+
+export default App;
---
+++ frontend/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './App.css';
+
+// Создаем корень и рендерим компонент App
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
