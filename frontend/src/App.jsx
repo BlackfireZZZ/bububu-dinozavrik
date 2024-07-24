dcf1da4d47b35d902303b4f9ec9805fc1a8348bf
BlackfireZZZ <Stipundrik@gmail.com>
2024-07-24 21:24:15 +0300

    New component Walk

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,12 +4,13 @@ import Timeline from "./components/Timeline";
 import RoundVideos from "./components/RoundVideos";
 import NewYear from "./components/NewYear";
 import FourteenthSlider from "./components/14thSlider";
+import Walk2 from "./components/Walk2";
 import './App.css';
 
 function App() {
     return (
         <div className="App">
-            <NewYear />
+            <Walk2 />
         </div>
     );
 }
