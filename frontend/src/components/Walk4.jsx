31a4d9bc2eae4691fb07b603b15362e13348a0a9
DrPepper18 <dbusygin70@gmail.com>
2024-07-30 11:54:12 +0300

    do_it_right

--- frontend/src/components/Walk4.jsx
+++ frontend/src/components/Walk4.jsx
@@ -3,9 +3,10 @@ import '../Begin.css';
 
 const Walk4 = () => {
     return (
-        <div className="walk4-container" style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', backgroundColor: 'green' }}>
+        <div className="walk4-container" 
+        style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', backgroundColor: 'green' }}>
             <div className="image-container walk4" style={{width: '30%'}}>
-                <img src={require('../assets/walk12.jpg')} alt="Walk 12" style={{ width: '100%', borderRadius: '20px' }} />
+                <img src={require('../assets/walk12.jpg')} alt="Walk 12" className='img4' style={{ width: '100%' }} />
             </div>
             <div className="content-container walk4">
                 <div className="block__text">
@@ -15,11 +16,11 @@ const Walk4 = () => {
                         то у меня было бы миллионов фоточек, чтобы сюда вставить, вот и думай, дурында.</p>
                 </div>
                 <div className="images-row" style={{ display: 'flex', justifyContent: 'space-between', width: '100%' }}>
-                    <img src={require('../assets/walk13.jpg')} alt="Walk 13" style={{ width: '49%', height: 'auto', borderRadius: '20px' }} />
-                    <img src={require('../assets/walk14.jpg')} alt="Walk 14" style={{ width: '49%', height: 'auto', borderRadius: '20px' }} />
+                    <img src={require('../assets/walk13.jpg')} alt="Walk 13" className='img4' />
+                    <img src={require('../assets/walk14.jpg')} alt="Walk 14" className='img4' />
                 </div>
                 <div className="image-full" style={{ width: '100%', marginTop: '10px' }}>
-                    <img src={require('../assets/walk15.jpg')} alt="Walk 15" style={{ width: '100%', height: 'auto', borderRadius: '20px' }} />
+                    <img src={require('../assets/walk15.jpg')} alt="Walk 15" className='img4' style={{ width: '100%' }} />
                 </div>
             </div>
         </div>
--- frontend/src/components/Walk5.jsx
+++ frontend/src/components/Walk5.jsx
@@ -7,11 +7,8 @@ const Walk5 = () => {
             display: 'flex', alignItems: 'center', 
             justifyContent: 'center', backgroundColor: 'bisque' 
         }}>
-            <div className="content-container walk5" style={{ }}>
-                <div className="images-row" style={{ 
-                    display: 'flex', justifyContent: 'space-between', 
-                    width: '100%' 
-                }}>
+            <div className="content-container walk5">
+                <div className="images-row row5">
                     <img src={require('../assets/walk16.jpg')} alt="Walk 16" className='img5' />
                     <img src={require('../assets/walk17.jpg')} alt="Walk 17" className='img5' />
                 </div>
@@ -19,8 +16,8 @@ const Walk5 = () => {
                     <img src={require('../assets/walk18.jpg')} alt="Walk 18" style={{ width: '100%', height: 'auto', borderRadius: '20px' }} />
                 </div>
             </div>
-            <div className="media-container walk5" style={{ }}>
-                <div className="image-video-row" style={{ display: 'flex', justifyContent: 'space-between', width: '100%', height: 'auto' }}>
+            <div className="media-container walk5">
+                <div className="image-video-row row5" style={{ height: 'auto' }}>
                     <img src={require('../assets/walk19.jpg')} alt="Walk 19" className='img5' />
                     <video src={require('../assets/walk19.mp4')} controls className='img5' />
                 </div>
