06452757a26ebe996561f26c555eee65903d4916
BlackfireZZZ <Stipundrik@gmail.com>
2024-08-13 21:55:16 +0300

    New shipSlider button

--- frontend/src/components/ShipSlider.jsx
+++ frontend/src/components/ShipSlider.jsx
@@ -4,6 +4,9 @@ import Ship2 from "./Ship2";
 import Ship3 from "./Ship3";
 import Ship4 from "./Ship4";
 import Ship5 from "./Ship5";
+import Right from '../assets/icons/ship-left.png'
+import Left from '../assets/icons/ship-right.png'
+
 
 const components = [
     { Component: Ship1, props: { text: "Текст первого компонента" } },
@@ -39,9 +42,13 @@ const ShipSlider = () => {
 
     return (
         <div className={`slider ${animating ? 'fade-out' : ''}`}>
-            <Component {...props} backgroundColor="#cfe5ff" />
-            <button className="arrow-button left" onClick={handlePrev}>&#8592;</button>
-            <button className="arrow-button right" onClick={handleNext}>&#8594;</button>
+            <Component {...props} backgroundColor="#cfe5ff"/>
+            <button className="arrow-button left" onClick={handlePrev}>
+                <img src={Left} alt="&#8592;" style={{width: '50px', height: 'auto'}}/>
+            </button>
+            <button className="arrow-button right" onClick={handleNext}>
+                <img src={Right} alt="&#8592;" style={{width: '50px', height: 'auto'}}/>
+            </button>
         </div>
     );
 };
