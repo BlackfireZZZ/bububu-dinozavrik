a56ba6af12d94f5097812358d7a3a4af7453978a
BlackfireZZZ <Stipundrik@gmail.com>
2024-08-16 18:10:05 +0300

    I wish, last fix

--- frontend/src/components/Walk7.jsx
+++ frontend/src/components/Walk7.jsx
@@ -4,14 +4,14 @@ import '../Walk3.css';
 
 const Walk7 = () => {
     return (
-        <div style={{ display: 'flex', justifyContent: 'center', backgroundColor: 'rgb(0, 0, 100)', minHeight: '100vh'}}>
-            <div style={{width: '30%', margin: '10px', padding: '10px', backgroundColor: 'rgb(255, 255, 255, 0.5)', borderRadius: '20px'}}>
-                <img src={require("../assets/walk24.jpg")} style={{width: '100%', borderRadius: '20px'}} alt="Small1"/>
-                <img src={require("../assets/walk27.jpg")} style={{width: '100%', borderRadius: '20px'}} alt="Small2"/>
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundColor: 'rgb(42,119,87)', height: '100vh' }}>
+            <div style={{ width: '25%', margin: '10px', padding: '10px', backgroundColor: 'rgb(255, 255, 255, 0.5)', borderRadius: '20px', display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
+                <img src={require("../assets/walk24.jpg")} style={{ width: '100%', borderRadius: '20px', marginBottom: '10px' }} alt="Small1" />
+                <img src={require("../assets/walk27.jpg")} style={{ width: '100%', borderRadius: '20px' }} alt="Small2" />
             </div>
-            <div style={{width: '30%', margin: '10px', padding: '10px', backgroundColor: 'rgb(255, 255, 255, 0.5)', borderRadius: '20px'}}>
-                <img src={require("../assets/walk25.jpg")} style={{width: '100%', borderRadius: '20px'}} alt="Small1"/>
-                <img src={require("../assets/walk26.jpg")} style={{width: '100%', borderRadius: '20px'}} alt="Small2"/>
+            <div style={{ width: '25%', margin: '10px', padding: '10px', backgroundColor: 'rgb(255, 255, 255, 0.5)', borderRadius: '20px', display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
+                <img src={require("../assets/walk25.jpg")} style={{ width: '100%', borderRadius: '20px', marginBottom: '10px' }} alt="Small1" />
+                <img src={require("../assets/walk26.jpg")} style={{ width: '100%', borderRadius: '20px' }} alt="Small2" />
             </div>
         </div>
     );
