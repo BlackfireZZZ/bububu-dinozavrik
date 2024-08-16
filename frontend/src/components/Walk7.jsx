77f8ee2de84d33e8d5451ae7f3a1d9c6a4364e21
DrPepper18 <dbusygin70@gmail.com>
2024-08-16 17:41:58 +0300

    its over

--- frontend/src/components/Walk7.jsx
+++ frontend/src/components/Walk7.jsx
@@ -5,11 +5,11 @@ import '../Walk3.css';
 const Walk7 = () => {
     return (
         <div style={{ display: 'flex', justifyContent: 'center', backgroundColor: 'rgb(0, 0, 100)', minHeight: '100vh'}}>
-            <div style={{width: '30%', margin: '10px'}}>
+            <div style={{width: '30%', margin: '10px', padding: '10px', backgroundColor: 'rgb(255, 255, 255, 0.5)', borderRadius: '20px'}}>
                 <img src={require("../assets/walk24.jpg")} style={{width: '100%', borderRadius: '20px'}} alt="Small1"/>
                 <img src={require("../assets/walk27.jpg")} style={{width: '100%', borderRadius: '20px'}} alt="Small2"/>
             </div>
-            <div style={{width: '30%', margin: '10px'}}>
+            <div style={{width: '30%', margin: '10px', padding: '10px', backgroundColor: 'rgb(255, 255, 255, 0.5)', borderRadius: '20px'}}>
                 <img src={require("../assets/walk25.jpg")} style={{width: '100%', borderRadius: '20px'}} alt="Small1"/>
                 <img src={require("../assets/walk26.jpg")} style={{width: '100%', borderRadius: '20px'}} alt="Small2"/>
             </div>
